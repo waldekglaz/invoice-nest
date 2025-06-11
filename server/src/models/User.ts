a998aeed4a47edf80e0eb551bad08a89e0b95ba3
Waldemar Glaz <waldekglaz@gmail.com>
2025-06-11 23:47:56 +0200

    UserSchema

---
+++ server/src/models/User.ts
@@ -0,0 +1,33 @@
+import mongoose, { Schema, Document } from 'mongoose';
+import bcrypt from 'bcrypt';
+
+interface IUser extends Document {
+    name: string;
+    email: string;
+    password: string;
+    comparePassword: (candidatePassword: string) => Promise<boolean>;
+}
+
+const userSchema: Schema<IUser> = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+}, { timestamps: true });
+
+userSchema.pre<IUser>('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword: string): Promise<boolean> {
+    return await bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model<IUser>('User', userSchema);
+
+export default User;
+
+
+
+
